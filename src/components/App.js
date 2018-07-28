c4c95a18b73543589662fd8d19bc9b3553c55a3c
khusroo hayat <khusroo.hayat@gmail.com>
2018-07-28 16:55:41 +0500

    index markup and main sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
 
 function Nav() {
-    return (
-        <nav className="menu show">
-        <div className="menu-branding show">
-          <div className="portrait show" />
-        </div>
-        <ul className="menu-nav show">
-          <li className="nav-item current show">
-            <a href="index.html" className="nav-link show">
-              Home
-            </a>
-          </li>
-        </ul>
-        <ul className="menu-nav show">
-          <li className="nav-item current show">
-            <a href="index.html" className="nav-link show">
-              About
-            </a>
-          </li>
-        </ul>
-      </nav>
-    )
+  return (
+    <nav className="menu">
+      <div className="menu-branding">
+        <div className="portrait" />
+      </div>
+      <ul className="menu-nav">
+        <li className="nav-item">
+          <a href="/" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/" className="nav-link">
+            About me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/" className="nav-link">
+            My Work
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/" className="nav-link">
+            How To Reach Me
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 function Header() {
@@ -37,15 +51,42 @@ function Header() {
   );
 }
 
-
+function Home() {
+  return (
+    <main id="home">
+      <h1 className="lg-heading">
+        John <span className="text-secondary">Doe</span>
+      </h1>
+      <h2 className="sm-heading">
+        Web Developer, Programmer And Open Source Enthusiast
+      </h2>
+      <div className="icons">
+        <a href="#!">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="#!">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="#!">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a href="#!">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+      </div>
+    </main>
+  );
+}
 
 class App extends Component {
+  componentDidMount() {
+    document.body.id = "bg-img";
+  }
   render() {
     return (
       <div>
-        {/* <Nav /> */}
         <Header />
-        <Nav />
+        <Home />
       </div>
     );
   }
